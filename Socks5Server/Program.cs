946ea5f80f3236f7068769114b898ebe08e63e89
SmRiley <jinmaoi@qq.com>
2019-12-11 22:55:49 +0800

    TCP基础实现

---
+++ Socks5Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Socks5Server.Core;
+using System.Net;
+using System.Threading.Tasks;
+namespace Socks5Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Socsk_Listen(IPAddress.Any, 1080);
+            Task.Delay(-1).Wait();
+        }
+
+        public static void PrintLog(string str)
+        {
+            Console.WriteLine(DateTime.Now + string.Format("{0}{1}", ":", str));
+        }
+    }
+}
